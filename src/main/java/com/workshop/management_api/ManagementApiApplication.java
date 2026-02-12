f10e553aa4620db3cf728f4a07574b926da16a05
Jerson Medina Lezama <jersonmedina2000@gmail.com>
2026-02-12 16:03:02 -0500

    first commit

---
+++ src/main/java/com/workshop/management_api/ManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementApiApplication.class, args);
+	}
+
+}
