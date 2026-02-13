b2e320b30d4c12fa6ef747f503efb35a291c0cfb
Jerson Medina Lezama <jersonmedina2000@gmail.com>
2026-02-13 11:16:59 -0500

    feat: basic creation of security config

---
+++ src/main/java/com/workshop/management_api/config/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.workshop.management_api.config.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.workshop.management_api.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+  private final UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return userRepository.findByUsername(username)
+      .orElseThrow(() -> 
+        new UsernameNotFoundException("User not found with username: ".concat(username))
+      );
+  }
+
+}
--- src/main/java/com/workshop/management_api/repository/UserRepository.java
+++ src/main/java/com/workshop/management_api/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.workshop.management_api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,6 @@ import com.workshop.management_api.domain.entity.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 
   // métodos personalizados
+  Optional<User> findByUsername(String username);
 
 }
