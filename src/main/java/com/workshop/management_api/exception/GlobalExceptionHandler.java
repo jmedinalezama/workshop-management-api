9958622b43cd371278e94b105242345a231574f4
Jerson Medina Lezama <jersonmedina2000@gmail.com>
2026-02-15 11:35:05 -0500

    feat: Exceptions were added to GlobalExceptions for CustomerRequest validations

--- src/main/java/com/workshop/management_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/workshop/management_api/exception/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.workshop.management_api.exception;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -28,6 +32,37 @@ public class GlobalExceptionHandler {
     return ResponseEntity
             .status(HttpStatus.UNAUTHORIZED)
             .body(ApiResponse.error(errorResponse));
+  }
+
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public ResponseEntity<ApiResponse<ErrorResponse>> handleHttpMessageNotReadableException(
+    HttpMessageNotReadableException ex
+  ) {
+    ErrorResponse errorResponse = new ErrorResponse(
+      HttpStatus.BAD_REQUEST.value(),
+      "Invalid JSON format. Please check your request body.",
+      LocalDateTime.now()
+    );
+
+    return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(ApiResponse.error(errorResponse));
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ApiResponse<Map<String, String>>> handleMethodArgumentNotValidException(
+    MethodArgumentNotValidException ex
+  ) {
+    
+    Map<String, String> errors = new HashMap<>();
+
+    ex.getBindingResult().getFieldErrors().forEach(error -> {
+      errors.put(error.getField(), error.getDefaultMessage());
+    });
+
+    return ResponseEntity
+              .status(HttpStatus.BAD_REQUEST.value())
+              .body(ApiResponse.error(errors));
   }
 
 }
